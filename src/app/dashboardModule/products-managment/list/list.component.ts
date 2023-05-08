5327628a8ecde2a7d28bb0dd9b34f91446d50906
Antoine FUSILIER <antoinefusilier@gmail.com>
2023-05-08 18:46:46 +0100

    product managment list

--- src/app/dashboardModule/products-managment/list/list.component.ts
+++ src/app/dashboardModule/products-managment/list/list.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
+import { AlertsService } from 'src/app/appServices/alerts.service';
 import { environment } from 'src/environments/environment.development';
 
 @Component({
@@ -15,15 +16,33 @@ export class ListComponent {
     "Content-Type" : "application/json",
     "Accept" : "application/json"
   })
-  constructor(private http: HttpClient){
-    this.http.post(`${environment.backEnd.cr_product}/get`, JSON.stringify({
+  constructor(
+    private http: HttpClient,
+    private as: AlertsService
+    ){
+      this.getProductsByDbPresta()
+  }
+
+  getProductsByDbPresta = () => {
+    this.http.get(`${environment.backEnd.cr_product}/get/${0}/${10}`,{})
+      .subscribe((v:any)=>{
+        console.log(v)
+        this.products = v;
+      },(error)=>{
+        this.as.error('Erreur aucun produit', 'Erreur lors de la récupération des produits :(...')
+        console.error(error)
+      })
+  }
+
+  getPrestaProductsByAPI = () => {
+    this.http.post(`${environment.backEnd.cr_product}/get`, {
       all: true,
       orderBy: '',
       orderType: '',
       offset: 0,
       limit: 10,
       name: ''
-    }),{headers: this.headers})
+    },{headers: this.headers})
     .subscribe((v:any)=>{
       v.forEach((e:any) => {
         e[71].elements[1].elements[0].attributes = String(Object.values(e[71].elements[1].elements[0].attributes)[0]).replace('etsleblanc.fr', 'ALXVADHMPDK9ZZXL46AHEP74UTZ8LNX5@etsleblanc.fr');
@@ -39,4 +58,5 @@ export class ListComponent {
       console.error(error)
     })
   }
+
 }
